475ec2f805fff12dd621ba9a3aba32b9c5bf550a
lunarian <donggongfu@gmail.com>
2018-07-10 22:05:51 +0800

    fix: This function declaration is not a prototype

--- src/Three20Core/Headers/TTDebug.h
+++ src/Three20Core/Headers/TTDebug.h
@@ -68,7 +68,7 @@
 #ifdef __cplusplus
   extern "C" __attribute__ ((visibility ("default"))) int TTIsInDebugger();
 #else
-  int TTIsInDebugger();
+  int TTIsInDebugger(void);
 #endif
 
   // We leave the __asm__ in this macro so that when a break occurs, we don't have to step out of
