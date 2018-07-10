a93592006b14b32a4c0ba45b753c2b46ab1f4d18
lunarian <donggongfu@gmail.com>
2018-07-10 22:50:23 +0800

    fix: replace deprecated enum

--- src/Three20Core/Sources/NSDateAdditions.m
+++ src/Three20Core/Sources/NSDateAdditions.m
@@ -50,9 +50,9 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 - (NSDate*)dateAtMidnight {
-	NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+	NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
 	NSDateComponents *comps = [gregorian components:
-                               (NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit)
+                               (NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay)
                                            fromDate:[NSDate date]];
 	NSDate *midnight = [gregorian dateFromComponents:comps];
 	[gregorian release];
@@ -236,7 +236,7 @@
   }
 
   NSCalendar* cal = [NSCalendar currentCalendar];
-  NSDateComponents* day = [cal components:NSDayCalendarUnit|NSMonthCalendarUnit|NSYearCalendarUnit
+  NSDateComponents* day = [cal components:NSCalendarUnitDay|NSCalendarUnitMonth|NSCalendarUnitYear
                                fromDate:self];
 
   if (day.day == today.day && day.month == today.month && day.year == today.year) {
