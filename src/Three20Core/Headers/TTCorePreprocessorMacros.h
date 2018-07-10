456ed0c657ad1fc4411188a4b7e06cf957b43896
lunarian <donggongfu@gmail.com>
2018-07-10 23:23:36 +0800

    refactor: TTCorePreprocessorMacros.h
    - 添加一个带消息参数的 deprecated 标记宏
    - update link

--- src/Three20Core/Headers/TTCorePreprocessorMacros.h
+++ src/Three20Core/Headers/TTCorePreprocessorMacros.h
@@ -24,12 +24,13 @@
  * __AVAILABILITY_INTERNAL_DEPRECATED         __attribute__((deprecated))
  */
 #define __TTDEPRECATED_METHOD __attribute__((deprecated))
+#define __TTDEPRECATED_MSG_ATTRIBUTE(msg) __attribute((deprecated((msg))))
 
 /**
  * Add this macro before each category implementation, so we don't have to use
  * -all_load or -force_load to load object files from static libraries that only contain
  * categories and no classes.
- * See http://developer.apple.com/library/mac/#qa/qa2006/qa1490.html for more info.
+ * See https://developer.apple.com/library/content/qa/qa1490/_index.html for more info.
  */
 #define TT_FIX_CATEGORY_BUG(name) @interface TT_FIX_CATEGORY_BUG_##name @end \
 @implementation TT_FIX_CATEGORY_BUG_##name @end
